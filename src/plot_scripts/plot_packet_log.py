eca2dd07553a94ab40e998793012035b5fd80ef7
GenetProj <genetproj@gmail.com>
2022-06-07 00:54:51 -0700

    Add plottting scripts.

---
+++ src/plot_scripts/plot_packet_log.py
@@ -0,0 +1,343 @@
+import argparse
+import csv
+import os
+from typing import Dict, List, Tuple, Optional
+
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import numpy as np
+
+from common.utils import pcc_aurora_reward
+from simulator.network_simulator.constants import BITS_PER_BYTE, BYTES_PER_PACKET
+from simulator.trace import Trace
+
+
+def parse_args():
+    """Parse arguments from the command line."""
+    parser = argparse.ArgumentParser("Plot packet log figures.")
+    parser.add_argument('--log-file', type=str, nargs="+", required=True,
+                        help="path to a testing log file.")
+    parser.add_argument('--save-dir', type=str, default=None,
+                        help="path to save.")
+    parser.add_argument('--trace-file', type=str, default=None,
+                        help="path to trace file.")
+
+    return parser.parse_args()
+
+
+class PacketLog():
+    def __init__(self, pkt_sent_ts: List[float], pkt_acked_ts: List[float],
+                 pkt_rtt: List[float], pkt_queue_delays: List[float],
+                 first_ts, binwise_bytes_sent: Dict[int, int],
+                 binwise_bytes_acked: Dict[int, int],
+                 binwise_bytes_lost: Dict[int, int], packet_log_file=None, ms_bin_size: int = 500):
+        self.pkt_log_file = packet_log_file
+        self.pkt_sent_ts = pkt_sent_ts
+        self.pkt_acked_ts = pkt_acked_ts
+        self.pkt_rtt = pkt_rtt
+        self.pkt_queue_delays = pkt_queue_delays
+        self.bin_size = ms_bin_size / 1000
+        self.first_ts = first_ts
+
+        self.binwise_bytes_sent = binwise_bytes_sent
+        self.binwise_bytes_acked = binwise_bytes_acked
+        self.binwise_bytes_lost = binwise_bytes_lost
+
+        self.avg_sending_rate = None
+        self.avg_throughput = None
+        self.avg_latency = None
+
+    @classmethod
+    def from_log_file(cls, packet_log_file: str, ms_bin_size: int = 500):
+        pkt_sent_ts = []
+        pkt_acked_ts = []
+        pkt_rtt = []
+        pkt_queue_delays = []
+        bin_size = ms_bin_size / 1000
+        first_ts = None
+
+        binwise_bytes_sent = {}
+        binwise_bytes_acked = {}
+        binwise_bytes_lost = {}
+        with open(packet_log_file, 'r') as f:
+            reader = csv.reader(f)
+            for line in reader:
+                if line[0] == 'timestamp':
+                    continue
+                ts = float(line[0])
+                pkt_id = int(line[1])
+                pkt_type = line[2]
+                pkt_byte = int(line[3])
+                if first_ts is None:
+                    first_ts = ts
+                # if ts - first_ts < 2:
+                #     continue
+                if pkt_type == 'acked':
+                    rtt = float(line[4]) * 1000
+                    queue_delay = float(line[5]) * 1000
+                    pkt_acked_ts.append(ts)
+                    pkt_rtt.append(rtt)
+                    pkt_queue_delays.append(queue_delay)
+
+                    bin_id = cls.ts_to_bin_id(ts, first_ts, bin_size)
+                    binwise_bytes_acked[bin_id] = binwise_bytes_acked.get(
+                        bin_id, 0) + pkt_byte
+                elif pkt_type == 'sent':
+                    pkt_sent_ts.append(ts)
+                    bin_id = cls.ts_to_bin_id(ts, first_ts, bin_size)
+                    binwise_bytes_sent[bin_id] = binwise_bytes_sent.get(
+                        bin_id, 0) + pkt_byte
+                elif pkt_type == 'lost':
+                    bin_id = cls.ts_to_bin_id(ts, first_ts, bin_size)
+                    binwise_bytes_lost[bin_id] = binwise_bytes_lost.get(
+                        bin_id, 0) + pkt_byte
+                elif pkt_type == 'arrived':
+                    pass
+                else:
+                    raise RuntimeError(
+                        "Unrecognized pkt_type {}!".format(pkt_type))
+        return cls(pkt_sent_ts, pkt_acked_ts, pkt_rtt, pkt_queue_delays,
+                   first_ts, binwise_bytes_sent, binwise_bytes_acked,
+                   binwise_bytes_lost, packet_log_file=packet_log_file,
+                   ms_bin_size=ms_bin_size)
+
+    @classmethod
+    def from_log(cls, pkt_log, ms_bin_size: int = 500):
+        pkt_sent_ts = []
+        pkt_acked_ts = []
+        pkt_rtt = []
+        pkt_queue_delays = []
+        bin_size = ms_bin_size / 1000
+        first_ts = None
+
+        binwise_bytes_sent = {}
+        binwise_bytes_acked = {}
+        binwise_bytes_lost = {}
+        first_ts = None
+        for line in pkt_log:
+            ts = line[0]
+            pkt_id = line[1]
+            pkt_type = line[2]
+            pkt_byte = line[3]
+            if first_ts is None:
+                first_ts = ts
+            if pkt_type == 'acked':
+                rtt = float(line[4]) * 1000
+                queue_delay = float(line[5]) * 1000
+                pkt_acked_ts.append(ts)
+                pkt_rtt.append(rtt)
+                pkt_queue_delays.append(queue_delay)
+
+                bin_id = cls.ts_to_bin_id(ts, first_ts, bin_size)
+                binwise_bytes_acked[bin_id] = binwise_bytes_acked.get(
+                    bin_id, 0) + pkt_byte
+            elif pkt_type == 'sent':
+                pkt_sent_ts.append(ts)
+                bin_id = cls.ts_to_bin_id(ts, first_ts, bin_size)
+                binwise_bytes_sent[bin_id] = binwise_bytes_sent.get(
+                    bin_id, 0) + pkt_byte
+            elif pkt_type == 'lost':
+                bin_id = cls.ts_to_bin_id(ts, first_ts, bin_size)
+                binwise_bytes_lost[bin_id] = binwise_bytes_lost.get(
+                    bin_id, 0) + pkt_byte
+            elif pkt_type == 'arrived':
+                pass
+            else:
+                raise RuntimeError(
+                    "Unrecognized pkt_type {}!".format(pkt_type))
+        return cls(pkt_sent_ts, pkt_acked_ts, pkt_rtt, pkt_queue_delays,
+                   first_ts, binwise_bytes_sent, binwise_bytes_acked,
+                   binwise_bytes_lost, packet_log_file=None,
+                   ms_bin_size=ms_bin_size)
+
+    @staticmethod
+    def ts_to_bin_id(ts, first_ts, bin_size) -> int:
+        return int((ts - first_ts) / bin_size)
+
+    @staticmethod
+    def bin_id_to_s(bin_id, bin_size) -> int:
+        return bin_id * bin_size
+
+    def get_throughput(self) -> Tuple[List[float], List[float]]:
+        throughput_ts = []
+        throughput = []
+        for bin_id in sorted(self.binwise_bytes_acked):
+            throughput_ts.append(self.bin_id_to_s(bin_id, self.bin_size))
+            throughput.append(
+                self.binwise_bytes_acked[bin_id] * BITS_PER_BYTE / self.bin_size / 1e6)
+        return throughput_ts, throughput
+
+    def get_sending_rate(self) -> Tuple[List[float], List[float]]:
+        sending_rate_ts = []
+        sending_rate = []
+        for bin_id in sorted(self.binwise_bytes_sent):
+            sending_rate_ts.append(self.bin_id_to_s(bin_id, self.bin_size))
+            sending_rate.append(
+                self.binwise_bytes_sent[bin_id] * BITS_PER_BYTE / self.bin_size / 1e6)
+        return sending_rate_ts, sending_rate
+
+    def get_rtt(self) -> Tuple[List[float], List[float]]:
+        return self.pkt_acked_ts, self.pkt_rtt
+
+    def get_queue_delay(self) -> Tuple[List[float], List[float]]:
+        return self.pkt_acked_ts, self.pkt_queue_delays
+
+    def get_loss_rate(self) -> float:
+        return 1 - len(self.pkt_acked_ts) / len(self.pkt_sent_ts)
+
+    def get_reward(self, trace_file: str, trace=None) -> float:
+        if trace_file and trace_file.endswith('.json'):
+            trace = Trace.load_from_file(trace_file)
+        elif trace_file and trace_file.endswith('.log'):
+            trace = Trace.load_from_pantheon_file(trace_file, 0, 50, 500)
+        loss = self.get_loss_rate()
+        if trace is None:
+            # original reward
+            return pcc_aurora_reward(
+                self.get_avg_throughput() * 1e6 / BITS_PER_BYTE / BYTES_PER_PACKET,
+                self.get_avg_latency() / 1e3, loss)
+        # normalized reward
+        return pcc_aurora_reward(
+            self.get_avg_throughput() * 1e6 / BITS_PER_BYTE / BYTES_PER_PACKET,
+            self.get_avg_latency() / 1e3, loss,
+            trace.avg_bw * 1e6 / BITS_PER_BYTE / BYTES_PER_PACKET,
+            trace.min_delay * 2 / 1e3)
+
+    def get_avg_sending_rate(self) -> float:
+        if not self.pkt_sent_ts:
+            return 0.0
+        if self.avg_sending_rate is None:
+            total_duration = self.pkt_sent_ts[-1] - self.pkt_sent_ts[0]
+            bytes_sum = 0
+            for _, bytes_sent in self.binwise_bytes_sent.items():
+                bytes_sum += bytes_sent
+            self.avg_sending_rate = bytes_sum * BITS_PER_BYTE / 1e6 / total_duration
+        return self.avg_sending_rate
+
+    def get_avg_throughput(self) -> float:
+        if not self.pkt_acked_ts:
+            return 0.0
+        if self.avg_throughput is None:
+            total_duration = self.pkt_acked_ts[-1] - self.pkt_acked_ts[0]
+            bytes_sum = 0
+            for _, bytes_acked in self.binwise_bytes_acked.items():
+                bytes_sum += bytes_acked
+            self.avg_throughput = bytes_sum * BITS_PER_BYTE / 1e6 / total_duration
+        return self.avg_throughput
+
+    def get_avg_latency(self) -> float:
+        if self.avg_latency is None:
+            self.avg_latency = np.mean(self.pkt_rtt)
+        return self.avg_latency
+
+
+def plot(trace: Optional[Trace], throughput_ts: List[float],
+         throughput: List[float], sending_rate_ts: List[float],
+         sending_rate: List[float], avg_tput: float, avg_sending_rate: float,
+         rtt_ts: List[float], rtt: List[float], avg_lat: float, pkt_loss: float,
+         reward: float, normalized_reward: float, save_dir: str, cc: str):
+    fig, axes = plt.subplots(2, 1, figsize=(6, 8))
+    axes[0].plot(throughput_ts, throughput, "-o", ms=2,  # drawstyle='steps-post',
+                 label='throughput, avg {:.3f}Mbps'.format(avg_tput))
+    axes[0].plot(sending_rate_ts, sending_rate, "-o", ms=2,  # drawstyle='steps-post',
+                 label='sending rate, avg {:.3f}Mbps'.format(avg_sending_rate))
+    if trace is not None:
+        axes[0].plot(trace.timestamps, trace.bandwidths, "-o", ms=2,  # drawstyle='steps-post',
+                     label='bandwidth, avg {:.3f}Mbps'.format(np.mean(trace.bandwidths)))
+        queue_size = trace.queue_size
+        trace_random_loss = trace.loss_rate
+        delay_noise = trace.delay_noise
+    else:
+        queue_size = "N/A"
+        trace_random_loss = "N/A"
+        delay_noise = "N/A"
+        # axes[0].plot(np.arange(30), np.ones_like(np.arange(30)) * 6, "-o", ms=2,  # drawstyle='steps-post',
+        #              label='bandwidth, avg {:.3f}Mbps'.format(6))
+    axes[0].legend()
+    axes[0].set_xlabel("Time(s)")
+    axes[0].set_ylabel("Rate(Mbps)")
+    axes[0].set_xlim(0, )
+    axes[0].set_ylim(0, )
+    if trace is not None:
+        axes[0].set_title('{} reward={:.3f}, normalized reward={:.3f}, gap={:.3f}'.format(
+            cc, reward, normalized_reward, trace.optimal_reward - normalized_reward))
+    else:
+        axes[0].set_title('{} reward={:.3f}, normalized reward={:.3f}'.format(
+            cc, reward, normalized_reward))
+
+    axes[1].plot(rtt_ts, rtt, ms=2, label='RTT, avg {:.3f}ms'.format(avg_lat))
+    # axes[1].plot(queue_delay_ts, queue_delay, label='Queue delay, avg {:.3f}ms'.format(np.mean(queue_delay)))
+    if trace is not None:
+        axes[1].plot(rtt_ts, np.ones_like(rtt) * 2 * trace.min_delay, c='C2',
+                     label="trace minRTT {:.3f}ms".format(2*min(trace.delays)))
+    axes[1].legend()
+    axes[1].set_xlabel("Time(s)")
+    axes[1].set_ylabel("Latency(ms)")
+    axes[1].set_title('{} loss={:.3f}, rand loss={:.3f}, queue={}, lat_noise={:.3f}'.format(
+        cc, pkt_loss, trace_random_loss, queue_size, delay_noise))
+    axes[1].set_xlim(0, )
+    # axes[1].set_ylim(0, )
+
+    plt.tight_layout()
+    if save_dir:
+        plt.savefig(os.path.join(save_dir, 'binwise_{}_plot.jpg'.format(cc)))
+    plt.close()
+
+
+def plot_pkt_log(trace, pkt_log, save_dir, cc):
+    sending_rate_ts, sending_rate = pkt_log.get_sending_rate()
+    throughput_ts, throughput = pkt_log.get_throughput()
+    rtt_ts, rtt = pkt_log.get_rtt()
+    # queue_delay_ts, queue_delay = pkt_log.get_queue_delay()
+    pkt_loss = pkt_log.get_loss_rate()
+    avg_tput = pkt_log.get_avg_throughput()
+    avg_sending_rate = pkt_log.get_avg_sending_rate()
+    avg_lat = pkt_log.get_avg_latency()
+    reward = pkt_log.get_reward("", None)
+    normalized_reward = pkt_log.get_reward("", trace)
+    plot(trace, throughput_ts, throughput, sending_rate_ts, sending_rate,
+         avg_tput, avg_sending_rate, rtt_ts, rtt, avg_lat, pkt_loss, reward,
+         normalized_reward, save_dir, cc)
+
+
+def main():
+    args = parse_args()
+    if args.trace_file and args.trace_file.endswith('.json'):
+        trace = Trace.load_from_file(args.trace_file)
+    elif args.trace_file and args.trace_file.endswith('.log'):
+        trace = Trace.load_from_pantheon_file(
+            args.trace_file, 0, 50, 500)
+    else:
+        trace = None
+
+    for log_idx, log_file in enumerate(args.log_file):
+        if not os.path.exists(log_file):
+            continue
+        pkt_log = PacketLog.from_log_file(log_file, 500)
+        cc = os.path.splitext(os.path.basename(log_file))[0].split('_')[0]
+
+        sending_rate_ts, sending_rate = pkt_log.get_sending_rate()
+        throughput_ts, throughput = pkt_log.get_throughput()
+        rtt_ts, rtt = pkt_log.get_rtt()
+        # queue_delay_ts, queue_delay = pkt_log.get_queue_delay()
+        pkt_loss = pkt_log.get_loss_rate()
+        avg_tput = pkt_log.get_avg_throughput()
+        avg_sending_rate = pkt_log.get_avg_sending_rate()
+        avg_lat = pkt_log.get_avg_latency()
+        reward = pkt_log.get_reward("", None)
+        normalized_reward = pkt_log.get_reward("", trace)
+        plot(trace, throughput_ts, throughput, sending_rate_ts, sending_rate,
+             avg_tput, avg_sending_rate, rtt_ts, rtt, avg_lat, pkt_loss,
+             reward, normalized_reward, args.save_dir, cc)
+
+        # if log_idx == 0:
+        #     print("{},{},{},{},{},".format(os.path.dirname(log_file),
+        #                                    np.mean(throughput), np.mean(rtt), loss, reward), end=',')
+        # else:
+        #     print("{},{},{},{},".format(np.mean(throughput),
+        #                                 np.mean(rtt), loss, reward), end=',')
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/plot_scripts/plot_time_series.py
@@ -0,0 +1,248 @@
+import argparse
+import csv
+import os
+from typing import Optional
+
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+from simulator.network_simulator.constants import BITS_PER_BYTE, BYTES_PER_PACKET
+from simulator.trace import Trace
+from common.utils import pcc_aurora_reward
+
+
+def parse_args():
+    """Parse arguments from the command line."""
+    parser = argparse.ArgumentParser("Plot time series figures.")
+    parser.add_argument('--log-file', type=str, nargs="+", required=True,
+                        help="path to a testing log file.")
+    parser.add_argument('--trace-file', type=str, default=None,
+                        help="path to a trace file.")
+    parser.add_argument('--save-dir', type=str, default=None,
+                        help="path to save.")
+    parser.add_argument('--noise', type=float, default=0)
+
+    args, unknown = parser.parse_known_args()
+    return args
+
+
+def plot(trace: Optional[Trace], log_file: str, save_dir: str, cc: str):
+    df = pd.read_csv(log_file)
+    assert isinstance(df, pd.DataFrame)
+    fig, axes = plt.subplots(6, 1, figsize=(12, 10))
+    axes[0].set_title(cc)
+    axes[0].plot(df['timestamp'], df['recv_rate'] / 1e6, 'o-', ms=2,
+            label='throughput, avg {:.3f}mbps'.format(
+                     df['recv_rate'].mean() / 1e6))
+    axes[0].plot(df['timestamp'], df['send_rate'] / 1e6, 'o-', ms=2,
+            label='send rate, avg {:.3f}mbps'.format(
+                     df['send_rate'].mean() / 1e6))
+
+    if trace:
+        avg_bw = trace.avg_bw
+        min_rtt = trace.min_delay * 2 / 1e3
+        axes[0].plot(trace.timestamps, trace.bandwidths, 'o-', ms=2, drawstyle='steps-post',
+                     label='bw, avg {:.3f}mbps'.format(avg_bw))
+    else:
+        axes[0].plot(df['timestamp'], df['bandwidth'] / 1e6,
+                     label='bw, avg {:.3f}mbps'.format(df['bandwidth'].mean() / 1e6))
+        avg_bw = df['bandwidth'].mean() / 1e6
+        min_rtt = None
+    axes[0].set_xlabel("Time(s)")
+    axes[0].set_ylabel("mbps")
+    axes[0].legend(loc='right')
+    axes[0].set_ylim(0, )
+    axes[0].set_xlim(0, )
+
+    axes[1].plot(df['timestamp'], df['latency']*1000,
+            label='RTT avg {:.3f}ms'.format(df['latency'].mean()*1000))
+    axes[1].set_xlabel("Time(s)")
+    axes[1].set_ylabel("Latency(ms)")
+    axes[1].legend(loc='right')
+    axes[1].set_xlim(0, )
+    axes[1].set_ylim(0, )
+
+    axes[2].plot(df['timestamp'], df['loss'],
+            label='loss avg {:.3f}'.format(df['loss'].mean()))
+    axes[2].set_xlabel("Time(s)")
+    axes[2].set_ylabel("loss")
+    axes[2].legend()
+    axes[2].set_xlim(0, )
+    axes[2].set_ylim(0, 1)
+
+    avg_reward_mi = pcc_aurora_reward(
+            df['recv_rate'].mean() / BITS_PER_BYTE / BYTES_PER_PACKET,
+            df['latency'].mean(), df['loss'].mean(),
+            avg_bw * 1e6 / BITS_PER_BYTE / BYTES_PER_PACKET, min_rtt)
+
+
+    axes[3].plot(df['timestamp'], df['reward'],
+            label='rewards avg {:.3f}'.format(avg_reward_mi))
+    axes[3].set_xlabel("Time(s)")
+    axes[3].set_ylabel("Reward")
+    axes[3].legend()
+    axes[3].set_xlim(0, )
+    # axes[3].set_ylim(, )
+
+    axes[4].plot(df['timestamp'], df['action'] * 1.0,
+                 label='delta avg {:.3f}'.format(df['action'].mean()))
+    axes[4].set_xlabel("Time(s)")
+    axes[4].set_ylabel("delta")
+    axes[4].legend()
+    axes[4].set_xlim(0, )
+
+    axes[5].plot(df['timestamp'], df['packet_in_queue'] /
+                 df['queue_size'], label='Queue Occupancy')
+    axes[5].set_xlabel("Time(s)")
+    axes[5].set_ylabel("Queue occupancy")
+    axes[5].legend()
+    axes[5].set_xlim(0, )
+    axes[5].set_ylim(0, 1)
+
+    # axes[5].plot(df['timestamp'], df['cwnd'], label='cwnd')
+    # axes[5].plot(df['timestamp'], df['ssthresh'], label='ssthresh')
+    # axes[5].set_xlabel("Time(s)")
+    # axes[5].set_ylabel("# packets")
+    # axes[5].set_ylim(0, df['cwnd'].max())
+    # axes[5].legend()
+    # axes[5].set_xlim(0, )
+    plt.tight_layout()
+    if save_dir is not None:
+        fig.savefig(os.path.join(save_dir, "{}_time_series.jpg".format(cc)))
+    plt.close()
+
+    # if log_idx == 0:
+    #     print("{},{},{},{},{},".format(os.path.dirname(log_file), df['recv_rate'].mean()/1e6,
+    #                                    df['latency'].mean()*1000,
+    #                                    df['loss'].mean(),
+    #                                    df['reward'].mean()), end='')
+    # else:
+    #     print("{},{},{},{},".format(df['recv_rate'].mean()/1e6,
+    #                                 df['latency'].mean()*1000,
+    #                                 df['loss'].mean(),
+    #                                 df['reward'].mean()), end='')
+    # if log_idx + 1 == len(args.log_file) and args.trace_file and args.trace_file.endswith('.log'):
+    #     trace = Trace.load_from_pantheon_file(args.trace_file, 50, 0, 50)
+    #     bw_avg = np.mean(trace.bandwidths)
+    #     bw_std = np.std(trace.bandwidths)
+    #     rtt_avg = np.mean(trace.delays)
+    #     T_s_bw, change_cnt = compute_T_s_bw(trace.timestamps, trace.bandwidths)
+    #     bw_range = max(trace.bandwidths) - min(trace.bandwidths)
+    #
+    #     print("{},{},{},{},{},{}".format(bw_avg, bw_std, T_s_bw, change_cnt, bw_range, rtt_avg))
+
+
+def parse_aurora_emulation_log(log_file: str):
+    """Parse aurora emulation MI log."""
+    timestamps = []
+    recv_rates = []
+    send_rates = []
+    latencies = []
+    loss_rates = []
+    rewards = []
+    actions = []
+    send_start_times = []
+    send_end_times = []
+
+    with open(log_file, 'r') as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            timestamps.append(float(row['timestamp']))
+            recv_rates.append(float(row['recv_rate']))
+            send_rates.append(float(row['send_rate']))
+            latencies.append(float(row['latency']))
+            loss_rates.append(float(row['loss']))
+            rewards.append(float(row['reward']))
+            actions.append(float(row['action']))
+            send_start_times.append(float(row['send_start_time']))
+            send_end_times.append(float(row['send_end_time']))
+
+    timestamps = np.array(timestamps)
+    recv_rates = np.array(recv_rates) / 1e6
+    send_rates = np.array(send_rates) / 1e6
+    latencies = np.array(latencies) * 1000
+    loss_rates = np.array(loss_rates)
+    rewards = np.array(rewards)
+    actions = np.array(actions)
+    send_start_times = np.array(send_start_times)
+    send_end_times = np.array(send_end_times)
+
+    return timestamps, recv_rates, send_rates, latencies, loss_rates, \
+        rewards, actions, send_start_times, send_end_times
+
+
+def plot_aurora_emulation_time_series(log_file: str, save_dir: str):
+    """Plot aurora MI level log from emulation/real world exp."""
+
+    timestamps, recv_rates, send_rates, latencies, loss_rates, rewards, \
+    actions, send_start_times, send_end_times = parse_aurora_emulation_log(log_file)
+
+    # df = pd.read_csv('test_aurora/aurora_emulation_log.csv')
+    fig, axes = plt.subplots(5, 1, figsize=(10, 10))
+    axes[0].plot(timestamps, recv_rates,
+                 label="Throughput avg {:.3f}Mbps".format(np.mean(recv_rates)))
+    axes[0].plot(timestamps, send_rates,
+                 label="Send rate avg {:.3f}Mbps".format(np.mean(send_rates)))
+    # axes[0].plot(np.arange(35), np.ones_like(
+    #     np.arange(35)) * 2, label='Link bandwidth')
+    axes[0].set_xlabel('Time(s)')
+    axes[0].set_ylabel('Mbps')
+    axes[0].legend()
+    # axes[0].set_ylim(0,  10)
+    axes[0].set_xlim(0, )
+
+    axes[1].plot(timestamps, latencies,
+                 label='RTT avg {:.3f}ms'.format(np.mean(latencies)))
+    axes[1].set_xlabel('Time(s)')
+    axes[1].set_ylabel('Latency(ms)')
+    axes[1].legend()
+    # axes[1].set_ylim(0, )
+    axes[1].set_xlim(0, )
+
+    axes[2].plot(timestamps, loss_rates,
+                 label='Loss avg {:.3f}'.format(np.mean(loss_rates)))
+    axes[2].set_xlabel('Time(s)')
+    axes[2].set_ylabel('Loss')
+    axes[2].legend()
+    axes[2].set_xlim(0, )
+    axes[2].set_ylim(0, 1)
+
+    axes[3].plot(timestamps, rewards,
+                 label='Reward avg {:.3f}'.format(np.mean(rewards)))
+    axes[3].set_xlabel('Time(s)')
+    axes[3].set_ylabel('Reward')
+    axes[3].legend()
+    axes[3].set_xlim(0, )
+
+    axes[4].plot(timestamps, actions, label='Action avg {:.3f}'.format(np.mean(actions)))
+    axes[4].set_xlabel('Time(s)')
+    axes[4].set_ylabel('Action')
+    axes[4].legend()
+    axes[4].set_xlim(0, )
+
+    plt.tight_layout()
+    fig.savefig(os.path.join(save_dir, "aurora_emulation.png"))
+
+
+def main():
+    args = parse_args()
+    for _, log_file in enumerate(args.log_file):
+        if not os.path.exists(log_file):
+            continue
+        if not args.trace_file:
+            trace = None
+        elif args.trace_file.endswith('.json'):
+            trace = Trace.load_from_file(args.trace_file)
+        elif args.trace_file.endswith('.log'):
+            trace = Trace.load_from_pantheon_file(args.trace_file, loss=0, queue=10)
+        else:
+            trace = None
+        cc = os.path.basename(log_file).split('_')[0]
+        plot(trace, log_file, args.save_dir, cc)
+
+
+if __name__ == "__main__":
+    main()
