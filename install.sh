52d8a5c2f6dd3017a7dce91e4a622df940ba724d
Zhengxu Xia <kevinxzx95@gmail.com>
2022-06-23 18:28:49 -0500

    Remove tinker dependency.

--- install.sh
+++ install.sh
@@ -7,9 +7,6 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     echo "Install dependencies for stable-baselines2..."
     sudo apt-get install -y cmake libopenmpi-dev python3-dev zlib1g-dev
 
-    echo "Install dependencies for matlibplot..."
-    sudo apt-get install -y python3-tk
-
     echo "Install mahimahi for emulation..."
     sudo apt-get -y install mahimahi
     sudo sysctl -w net.ipv4.ip_forward=1
